4d28c704d619479f23712281cd9d569fb8c923d6
littlepy <mail@littlepy.com>
2019-10-06 01:52:53 +0300

    update hooks

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageThreshold: {
     global: {
-      branches: 90,
+      branches: 85,
       functions: 90,
       lines: 90,
       statements: 90,
